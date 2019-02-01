6086f493fb8131c2b33a4087bbe9bea6a42582b2
janycephoa <janycephoa@gmail.com>
2019-02-01 11:20:29 +0800

    Module 5 completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,12 +13,25 @@ class Quiz extends Component {
       quiz_position: 1
     }
   }
+
+  showNextQuestion() {
+    this.setState((prevState) => {
+      return {quiz_position: prevState.quiz_position + 1}
+    })
+  }
+
+  // showNextQuestion() {
+  //   this.setState((prevState) => {
+  //     return {quiz_position: prevState.quiz_position + 1}
+  //   })
+  // }
+
   render() {
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
     return (
       <div>
         {isQuizEnd ? <QuizEnd /> :
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
         }
       </div>
     )
